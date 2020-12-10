38dcedbe7810dbb2ef20b9c86dfaacfc9d838a5f
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-12-10 13:11:25 -0500

    lecture work-in-progress

--- review/src/reducers/index.js
+++ review/src/reducers/index.js
@@ -9,12 +9,22 @@ export const reducer = (state, action) => {
     switch(action.type) {
         case("CHANGE_NAME"):
             return({
+                ...state,
                 person: {
                     ...person,
                     name: action.payload
                 }
             });
+            
         case("CHANGE_LOCATION"):
+            return({
+                ...state,
+                person: {
+                    ...person,
+                    location: action.payload
+                }
+            });
+
         default:
             return(state);
     }
