87336ea0194558ced3a1407b07c36e493c91f4f1
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-12-10 12:59:27 -0500

    lecture work-in-progress

--- review/src/App.js
+++ review/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, createContext, useContext } from 'react';
 import data from './data';
 
 const PersonContext = createContext();
+const DogContext = createContext();
 
 const App = ()=> {
     const [person, setPerson] = useState(data);
@@ -9,7 +10,7 @@ const App = ()=> {
     const contextData = { person:person, setPerson:setPerson };
 
     return(<div className="component">
-        <PersonContext.Provider value={ contextData }>  
+        <PersonContext.Provider value={[person, setPerson]}>
             <h1>Hello World</h1>
             <SubComponent1 />
         </PersonContext.Provider>
@@ -17,54 +18,54 @@ const App = ()=> {
 };
 
 const SubComponent1 = ()=> {
-    const data = useContext(PersonContext);
-    const { name } = data.person;
+    const [ person, setPerson] = useContext(PersonContext);
+    const { name } = person;
 
     return(<div className="component">
         <p><strong>Name:</strong> { name.title } { name.first} { name.last}</p>
-        {/* <SubComponent2 /> */}
+        <SubComponent2 />
     </div>);
 };
 
-// const SubComponent2 = ()=> {
-//     const [ person ] = useContext(PersonContext);
-//     const { location } = person;
+const SubComponent2 = ()=> {
+    const [ person ] = useContext(PersonContext);
+    const { location } = person;
 
-//     return(<div className="component">
-//         <p><strong>Location:</strong> {location.street} {location.city} {location.state}</p>
-//         <SubComponent3 />
-//     </div>);
-// };
+    return(<div className="component">
+        <p><strong>Location:</strong> {location.street} {location.city} {location.state}</p>
+        <SubComponent3 />
+    </div>);
+};
 
-// const SubComponent3 = ()=> {
-//     const [person, setPerson] = useContext(PersonContext);
+const SubComponent3 = ()=> {
+    const [person, setPerson] = useContext(PersonContext);
 
-//     const handleNameChange = ()=> {
-//         setPerson({
-//             ...person,
-//             name: {
-//                 title: "Mr",
-//                 first: "Warren",
-//                 last: "Longmire"
-//             }
-//         });
-//     }
+    const handleNameChange = ()=> {
+        setPerson({
+            ...person,
+            name: {
+                title: "Mr",
+                first: "Warren",
+                last: "Longmire"
+            }
+        });
+    }
 
-//     const handleLocationChange = () => {
-//         setPerson({
-//             ...person,
-//             location:{
-//                 street: "22 N 22nd street",
-//                 city: "Philadelphia",
-//                 state: "PA"
-//             }
-//         })
-//     }
+    const handleLocationChange = () => {
+        setPerson({
+            ...person,
+            location:{
+                street: "22 N 22nd street",
+                city: "Philadelphia",
+                state: "PA"
+            }
+        })
+    }
 
-//     return(<div className="component">
-//         <button onClick={handleNameChange}>Set Name</button>
-//         <button onClick={handleLocationChange}>Set Location</button>
-//     </div>);
-// };
+    return(<div className="component">
+        <button onClick={handleNameChange}>Set Name</button>
+        <button onClick={handleLocationChange}>Set Location</button>
+    </div>);
+};
 
 export default App;
